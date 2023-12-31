70235e8cbbdd528f3e0c880aff2474c041107364
Anne Chepkeitany <rerimoianne.4@gmail.com>
2023-12-31 05:57:41 +0000

    Introduce asserts with a few test cases

--- sum_array.py
+++ sum_array.py
@@ -7,3 +7,11 @@ def sum_array(items, i=0):
     if i == len(items):
         return 0
     return items[i] + sum_array(items, i + 1)
+
+if __name__ == "__main__":
+    assert sum_array([2, 4, 6, 1, 3, 5]) == 21, "Test with positive numbers failed"
+    assert sum_array([]) == 0, "Test with empty array failed"
+    assert sum_array([-1, -2, -3, -4]) == -10, "Test with negative numbers failed"
+    assert sum_array([10]) == 10, "Test with single element failed"
+
+    print("All tests passed!")
