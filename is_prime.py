adfb49ff209d39a45a9f0e26c0237cb522f6c040
Anne Chepkeitany <rerimoianne.4@gmail.com>
2024-01-01 07:02:10 +0000

    Add a function to check if a number is prime

---
+++ is_prime.py
@@ -0,0 +1,24 @@
+'''
+Check if a given number is prime or not
+'''
+
+def is_prime(number):
+    if number < 2:
+        return False
+    if number == 2:
+        return True
+
+    square_root_value = number ** 0.5
+    return check_divisors(number, 2, square_root_value)
+
+
+'''
+Check if a given number has any divisor from 2 to the square root of 2
+'''
+def check_divisors(number, divisor, square_root_value):
+    if divisor > square_root_value:
+        return True # no divisor found from 2 to square_root_value
+    if number % divisor == 0:
+        return False
+    return check_divisors(number, divisor + 1, square_root_value)
+
