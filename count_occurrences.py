f29c09fa2695fe01b25fe8069f0c81757d2db2ea
Anne Chepkeitany <rerimoianne.4@gmail.com>
2024-01-02 07:17:37 +0000

    Add time and space complexity analysis

--- count_occurrences.py
+++ count_occurrences.py
@@ -4,8 +4,17 @@ Given an array of numbers and a key, count the frequency of the key
 def count_occurrences(arr, key):
     return count_occurrences_helper(arr, key)
 
+'''
+Helper function to count the frequencies of a key in an array
+Time Complexity :  O(n)   - where n is the size of the input array
+                            The recursive function goes through 
+                            all the elements in the array once
+Space Complexity :  O(n)  - There are n recursive calls until 
+                            the base case is reached. Each call consumes
+                            additional space on the call stack
+'''
 def count_occurrences_helper(arr, key, index=0):
-    if index >= len(arr):
+    if index >= len(arr): # base case
         return 0
 
     count = 1 if arr[index] == key else 0
