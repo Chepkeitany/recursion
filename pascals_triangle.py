4f6cf0a9df8f2e7cca5eac0c7abf3d98a7c85cbe
Anne Chepkeitany <rerimoianne.4@gmail.com>
2024-01-03 06:14:14 +0000

    Add time and space complexity analysis

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -7,6 +7,19 @@ Each row of Pascal's Triangle represents the coefficients in the binomial expans
 The triangle is constructed starting with the number 1 at the top. 
 Each number below it is the sum of the two numbers directly above it. 
 The sides of the triangle are always 1.
+
+Time Complexity: O(n^2) - where n if the number of rows. The function makes n
+                          recursive calls until the base case is reached. In each call,
+                          we also construct a new row based on the previous row which takes i time
+                          depending on the number of elements in that row which is n for the last row
+                          The time taken for all rows from 1 to num_rows is given by 
+                          the following arithmetic progression formula:
+                          num_rows (num_rows + 1) / 2 which can be simplified as num_rows^2
+Space Complexity: O(n^2) - Since we store a list of each row of the pascal triangle,
+                           the space required is equal given the arithmetic formula above.
+                           Therefore, space required is num_rows ^2. The recursive call stack is O(num_rows)
+                           because we make n recursive calls until the base case is reached.
+                           The dominant power in the space complexity is num_rows ^ 2                 
 '''
 
 def pascal_triangle(num_rows):
