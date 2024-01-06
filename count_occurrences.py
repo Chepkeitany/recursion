461b9895abe7a034f50298fb5495966240ac3267
Anne Chepkeitany <rerimoianne.4@gmail.com>
2024-01-06 06:38:54 +0000

    Fix linting in count_occurrences function

--- count_occurrences.py
+++ count_occurrences.py
@@ -1,20 +1,24 @@
 '''
 Given an array of numbers and a key, count the frequency of the key
-'''
-def count_occurrences(arr, key):
-    return count_occurrences_helper(arr, key)
 
-'''
-Helper function to count the frequencies of a key in an array
+It uses a helper function to count the frequency of a key in an array
 Time Complexity :  O(n)   - where n is the size of the input array
-                            The recursive function goes through 
+                            The recursive function goes through
                             all the elements in the array once
-Space Complexity :  O(n)  - There are n recursive calls until 
+Space Complexity :  O(n)  - There are n recursive calls until
                             the base case is reached. Each call consumes
                             additional space on the call stack
 '''
+
+
+def count_occurrences(arr, key):
+    """ Given an array of numbers and a key, count the frequency of the key"""
+    return count_occurrences_helper(arr, key)
+
+
 def count_occurrences_helper(arr, key, index=0):
-    if index >= len(arr): # base case
+    """ Helper function to count the frequency of a key in the array"""
+    if index >= len(arr):  # base case
         return 0
 
     count = 1 if arr[index] == key else 0
@@ -23,8 +27,12 @@ def count_occurrences_helper(arr, key, index=0):
 
     return total_count
 
-assert count_occurrences([2, 4, 6, 8, 4], 4) == 2, "Failed on count_occurrences([2, 4, 6, 8, 4], 4)"
-assert count_occurrences([2, 4, 6, 8, 4], 2) == 1, "Failed on count_occurrences([2, 4, 6, 8, 4], 2)"
-assert count_occurrences([2, 2, 2, 2, 4], 2) == 4, "Failed on count_occurrences([2, 2, 2, 2, 4], 2)"
+
+assert count_occurrences(
+    [2, 4, 6, 8, 4], 4) == 2, "Failed on count_occurrences([2, 4, 6, 8, 4], 4)"
+assert count_occurrences(
+    [2, 4, 6, 8, 4], 2) == 1, "Failed on count_occurrences([2, 4, 6, 8, 4], 2)"
+assert count_occurrences(
+    [2, 2, 2, 2, 4], 2) == 4, "Failed on count_occurrences([2, 2, 2, 2, 4], 2)"
 
 print("All tests passed!")
