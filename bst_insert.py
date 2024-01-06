1c70e17e89f1d9a9ee6a8273b961e378aa1a6319
Anne Chepkeitany <rerimoianne.4@gmail.com>
2024-01-06 07:17:53 +0000

    Fix linting in bst_insert function

--- bst_insert.py
+++ bst_insert.py
@@ -1,25 +1,40 @@
-# TreeNode class
-class TreeNode:
-    def __init__(self, value):
-       self.value = value
-       self.left = None
-       self.right = None
-
+# pylint: disable=R0903
+# pylint: disable=W0621
 '''
 Insert a node into a BST following the BST properties
 For any given node with a key, all keys in the left subtree are less than the node's key,
 and all keys in the right subtree are greater than the node's key.
 This property must hold true for every node in the tree.
 
-Time Complexity: O(h) where h is the height of bst. In the worst case, 
+Time Complexity: O(h) where h is the height of bst. In the worst case,
                       the bst looks like a linked list so h is equal to
                       the number of nodes in the bst. So the time complexity
                       becomes O(n). For a balanced bst, time complexity is
                       O(log n)
 Space Complexity: O(h) The function will make h recursive calls until it finds
                        a location to insert the new node.
+
+Traversal of the BST uses in order traversal method:
+Time Complexity: O(n)  - where n is the number of nodes that we will visit in the tree
+Space Complexity: O(h) - where h is the height of the tree. We will make
+                        h recursive calls as we're traversing the tree in order
+                        in a balanced tree, this will be logarithmic but
+                        in a tree that is skewed to one side and looks like a
+                        linked list, this will lead to n recursive calls
 '''
+
+
+class TreeNode:
+    """ A binary TreeNode class """
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
 def insert_bst(root, value):
+    """ Insert a node into a BST following the BST properties """
     if not root:
         return TreeNode(value)
     if value < root.value:
@@ -29,29 +44,24 @@ def insert_bst(root, value):
 
     return root
 
-def print_bst_inorder(node):
-    if not node:
-        return
 
-    print_bst_inorder(node.left)
-    print(node.value, end=" ")
-    print_bst_inorder(node.right)
+# def print_bst_inorder(node):
+#     if not node:
+#         return
 
-'''
-Traverse BST inorder and return the node values in the order they were visited
-Time Complexity: O(n)  - where n is the number of nodes that we will visit in the tree
-Space Complexity: O(h) - where h is the height of the tree. We will make 
-                         h recursive calls as we're traversing the tree in order
-                         in a balanced tree, this will be logarithmic but 
-                         in a tree that is skewed to one side and looks like a
-                         linked list, this will lead to n recursive calls
-'''
+#     print_bst_inorder(node.left)
+#     print(node.value, end=" ")
+#     print_bst_inorder(node.right)
+
+
 def inorder_traversal(node, inorder_list):
+    """ Traverse BST inorder and return the node values in the order they were visited """
     if not node:
         return
     inorder_traversal(node.left, inorder_list)
     inorder_list.append(node.value)
     inorder_traversal(node.right, inorder_list)
+
 
 # Initialize BST
 root = TreeNode(6)
@@ -65,12 +75,14 @@ root.right.right = TreeNode(9)
 root = insert_bst(root, 4)
 inorder_list = []
 inorder_traversal(root, inorder_list)
-assert inorder_list == [1, 3, 4, 5, 6, 8, 9], "Failed to correctly insert 4 into the bst"
+assert inorder_list == [1, 3, 4, 5, 6, 8,
+                        9], "Failed to correctly insert 4 into the bst"
 
 # Insert 7
 root = insert_bst(root, 7)
 inorder_list = []
 inorder_traversal(root, inorder_list)
-assert inorder_list == [1, 3, 4, 5, 6, 7, 8, 9], "Failed to correctly insert 7 into the bst"
+assert inorder_list == [1, 3, 4, 5, 6, 7, 8,
+                        9], "Failed to correctly insert 7 into the bst"
 
 print("All tests passed!")
