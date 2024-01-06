11776409eb0e877e358bb503cfe444e26ab37a81
Anne Chepkeitany <rerimoianne.4@gmail.com>
2024-01-06 06:43:27 +0000

    Fix linting in factorial function

--- factorial.py
+++ factorial.py
@@ -6,15 +6,20 @@ Space Complexity: O(n) - the maximum recursive depth is n. Each recursive call
                          adds a new call stack frame
                          consuming additional space on the call stack
 '''
-def factorial(n):
-    if n < 0:
-        raise ValueError("Factorial can only be computed for non-negative numbers!")
-    elif n <= 1:
-        return 1
-    else:
-        return n * factorial(n-1)
 
-if __name__ == "__main__":
-    print(factorial(5))
-    print(factorial(1))
-    print(factorial(0))
+
+def factorial(n):
+    """ Compute the factorial of a positive number """
+    if n < 0:
+        raise ValueError(
+            "Factorial can only be computed for non-negative numbers!")
+    if n <= 1:
+        return 1
+    return n * factorial(n - 1)
+
+
+assert factorial(5) == 120, "Failed on factorial(5)"
+assert factorial(1) == 1, "Failed on factorial(1)"
+assert factorial(0) == 1, "Failed on factorial(0)"
+
+print("All tests passed!")
