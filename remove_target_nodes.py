5a776adab653df31c78be65105648495077368bd
Anne Chepkeitany <rerimoianne.4@gmail.com>
2024-01-08 06:07:01 +0000

    Add one more test case

--- remove_target_nodes.py
+++ remove_target_nodes.py
@@ -9,11 +9,14 @@ Space Complexity: O(n) - n is the number of recursive calls in the stack until
                          additional space in the call stack.
 '''
 
+
 class Node:
     """Class to represent a node in a linked list"""
+
     def __init__(self, data):
         self.data = data
         self.next = None
+
 
 def remove_target_nodes(node, target):
     """Remove target nodes from the linked list"""
@@ -28,6 +31,7 @@ def remove_target_nodes(node, target):
         return node.next
     return node
 
+
 def print_list(node):
     """Print out the values of the linked list"""
     current = node
@@ -35,6 +39,18 @@ def print_list(node):
         print(current.data, end=" ")
         current = current.next
     print()
+
+
+def get_list_values(node):
+    """Return a list of the values of the linked list in order"""
+    result = []
+    current = node
+    while current:
+        result.append(current.data)
+        current = current.next
+
+    return result
+
 
 # Initiliaze linked list 1
 head1 = Node(1)
@@ -45,4 +61,17 @@ head1.next.next.next.next = Node(3)
 
 print_list(head1)
 new_list_head = remove_target_nodes(head1, 2)
-print_list(new_list_head)
+assert get_list_values(new_list_head) == [
+    1, 5, 4, 3], "Failed to remove node with value 2"
+
+head2 = Node(5)
+head2.next = Node(4)
+head2.next.next = Node(2)
+head2.next.next.next = Node(3)
+head2.next.next.next.next = Node(1)
+
+print_list(head2)
+assert get_list_values(remove_target_nodes(head2, 3)) == [
+    5, 4, 2, 1], "Failed to remove node with value 3"
+
+print("All tests passed!")
